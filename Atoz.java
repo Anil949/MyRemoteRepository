8d799fd544d135b460d5d7378d6e505b0605f91b
Soubhagyapu Anil <soubhagyapu.anil@ibm.com>
2021-06-30 11:24:22 +0530

    created Atoz,modified xyz

---
+++ Atoz.java
@@ -0,0 +1,8 @@
+public class Atoz
+{
+	public static void main(String args[])
+	{
+		System.out.println("hey");
+	}
+}
+
--- Xyz.java
+++ Xyz.java
@@ -3,6 +3,7 @@ public class xyz
 	public static void main(String args[])
 	{
 		System.out.println("zyx");
+                System.out.println("hello");
 	}
 }
 
